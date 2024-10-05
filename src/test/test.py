b8ba87240f8b61d985ee8c244ccafb6b9aeafbb7
Emil <emno@itu.dk>
2024-10-05 16:15:10 +0200

    solve linting problems

--- src/test/test.py
+++ src/test/test.py
@@ -1,9 +1,11 @@
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self) -> None:
         self.assertEqual('foo'.upper(), 'FOO')
+
 
 if __name__ == '__main__':
     unittest.main()
