f9307e82d899f2eeced72d2a890b6c3b7e951165
Mikkel Bistrup Andersen <mbia@itu.dk>
2024-10-10 18:35:49 +0200

    finished hospital.go

--- src/Hospital/hospital.go
+++ src/Hospital/hospital.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	types "SEC-miniproject2/src/Hospital/Types"
+	utilities "SEC-miniproject2/src/Hospital/Utilities"
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 var client *http.Client
@@ -19,7 +24,7 @@ var data int
 var recshares int
 
 func patientHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println(port, "/patiens received")
+	log.Println(port, "/patients received")
 	if r.Method == "POST" {
 		log.Println(port, "/patients POST received")
 		body, err := io.ReadAll(r.Body)
@@ -69,4 +74,75 @@ func sendport() {
 		log.Println(port, ": Sent port to ", p, ". Received response", resp.Status)
 
 	}
+}
+
+func shareHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		log.Println(port, "/share received")
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(port, "Error reading body: ", err)
+			w.WriteHeader(400)
+			return
+		}
+		recshare := &types.Share{}
+		err = json.Unmarshal(body, recshare)
+		if err != nil {
+			log.Fatal(port, "Error unmarshalling: ", err)
+			w.WriteHeader(400)
+			return
+		}
+		data += recshare.Share
+		recshares++
+		log.Println(port, "Received share: ", recshare.Share)
+
+		if recshares == patientstotal {
+			log.Println("Final value is: ", data)
+		}
+		w.WriteHeader(200)
+
+	}
+}
+
+func hosserver() {
+	log.Println(port, ": Starting server")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/patients", patientHandler)
+	mux.HandleFunc("/share", shareHandler)
+
+	err := http.ListenAndServeTLS(utilities.PortToString(port), "server.crt", "server.key", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	flag.IntVar(&port, "port", 80, "Port number")
+	flag.IntVar(&patientstotal, "t", 3, "Total number of patients")
+	flag.Parse()
+
+	data = 0
+	certificate, err := os.ReadFile("server.crt")
+	if err != nil {
+		log.Fatal("Error reading certificate: ", err)
+	}
+
+	certificatepool := x509.NewCertPool()
+	certificatepool.AppendCertsFromPEM(certificate)
+
+	client = &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: certificatepool,
+			},
+		},
+	}
+
+	go hosserver()
+
+	for {
+	}
+
 }
